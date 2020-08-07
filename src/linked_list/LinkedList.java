750f911d23b3d48368b05dd7d11033f918162f5d
Barry Tsai <Barry_Tsai@wistron>
2020-08-07 11:50:58 +0800

    build link list

---
+++ src/linked_list/LinkedList.java
@@ -0,0 +1,80 @@
+package linked_list;
+
+
+public class LinkedList {
+    Node head; // head of list
+
+    /* Linked list Node.  This inner class is made static so that
+       main() can access it */
+    static class Node {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        } // Constructor
+    }
+
+    /* method to create a simple linked list with 3 nodes*/
+    public static void main(String[] args)
+    {
+        /* Start with the empty list. */
+        LinkedList llist = new LinkedList();
+
+        llist.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+
+        /* Three nodes have been allocated dynamically.
+          We have references to these three blocks as head,
+          second and third
+
+          llist.head        second              third
+             |                |                  |
+             |                |                  |
+         +----+------+     +----+------+     +----+------+
+         | 1  | null |     | 2  | null |     |  3 | null |
+         +----+------+     +----+------+     +----+------+ */
+
+        llist.head.next = second; // Link first node with the second node
+
+        /*  Now next of the first Node refers to the second.  So they
+            both are linked.
+
+         llist.head        second              third
+            |                |                  |
+            |                |                  |
+        +----+------+     +----+------+     +----+------+
+        | 1  |  o-------->| 2  | null |     |  3 | null |
+        +----+------+     +----+------+     +----+------+ */
+
+        second.next = third; // Link second node with the third node
+
+        /*  Now next of the second Node refers to third.  So all three
+            nodes are linked.
+
+         llist.head        second              third
+            |                |                  |
+            |                |                  |
+        +----+------+     +----+------+     +----+------+
+        | 1  |  o-------->| 2  |  o-------->|  3 | null |
+        +----+------+     +----+------+     +----+------+ */
+
+        third.next = fourth;
+
+        llist.printlist();
+    }
+
+    public void printlist(){
+
+        Node n = head;
+        // 判斷是否有下一個項目
+        while (n!= null){
+            System.out.println(n.data);
+            n = n.next;
+        }
+
+    }
+}
